29205962b894ae5d77138c9db35428afa3ceb742
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-03 18:05:01 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- Day5/p2 (new)/square_root.py
+++ Day5/p2 (new)/square_root.py
@@ -1,8 +1,16 @@
 '''Write a python program to find the square root of the given number'''
 # using approximation method
 
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
 def main():
-    '''square root'''
+    '''approximation method'''
     val = int(input())
     epsilon = 0.01
     res = 0.0
@@ -13,7 +21,5 @@ def main():
         print("Failed  on square root of", val)
     else:
         print(res)
-    #your code here
-
 if __name__ == "__main__":
     main()
