db9f63fae6319fcdcacf7ffe810d0df9a9906f08
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-03 15:19:04 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- Day5/p2/square_root.py
+++ Day5/p2/square_root.py
@@ -1,9 +1,22 @@
-# Write a python program to find the square root of the given number 
+'''Write a python program to find the square root of the given number'''
 # using approximation method
 
 def main():
-	s = raw_input()
-	#your code here
+    '''square root'''
+    val = int(input())
+    epsilon = 0.01
+    guess = 0.0
+    increment = 0.0001
+    num = 0
+    while abs(guess**2 -val) >= epsilon:
+        guess += increment
+        num += 1
+    print("num = ", num)
+    if abs(guess**2 - val) >= epsilon:
+        print("Failed  on square root of", val)
+    else:
+        print(guess, 'is close to the squareroot of', val)
+    #your code here
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
--- Day5/p3/square_root_bisection.py
+++ Day5/p3/square_root_bisection.py
@@ -1,4 +1,4 @@
-# Write a python program to find the square root of the given number
+'''Write a python program to find the square root of the given number'''
 # using approximation method
 
 # testcase 1
@@ -10,12 +10,25 @@
 # output: 6.999999999999991
 
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
+    '''Bisection method'''
+    value = int(input())
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    count = 0
+    low = 1.0
+    high = value
+    res = high+low/2.0
+    while abs(res**2 - value) >= epsilon:
+        count += 1
+        if res**2 < value:
+            low = res
+        else:
+            high = res
+        res = (high+low)/2.0
+    print(count)
+    print(res)
+    # your code starts here
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
