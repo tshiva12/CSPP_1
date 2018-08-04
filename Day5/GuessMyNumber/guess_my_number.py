1e426cd8e8760d6eb32eee22320d0403266c7816
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-04 16:52:12 +0530

    CSPP_1

--- Day5/GuessMyNumber/guess_my_number.py
+++ Day5/GuessMyNumber/guess_my_number.py
@@ -1,8 +1,22 @@
-#Guess My Number Exercise
+'''Guess My Number Exercise'''
 
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    '''lucky number'''
+    mid = 50
+    high = 100
+    low = 0
+    #luckynum = 0
+    num = 'l'
+    while num != 'c':
+        print(mid)
+        num = input("enter 'h' if guess is too high,'l' if its too low.\
+            'c' to indicate I guessed correctly")
+        if num == 'h':
+            high = mid
+            mid = (high+low)//2
+        elif num == 'l':
+            low = mid
+            mid = (high+low)//2
+    print('your lucky number is :', mid)
+if __name__ == "__main__":
+    main()
