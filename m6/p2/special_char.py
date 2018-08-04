286a2eb0772c2689cef37dc2152c2597f7747657
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-04 11:59:30 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- m6/p2/special_char.py
+++ m6/p2/special_char.py
@@ -8,13 +8,15 @@ def main():
     Read string from the input, store it in variable str_input.
     '''
     string = input()
+    string1 = ['!', '@', '#', '$', '%', '^', '&', '*']
     newstr = ""
     len1 = len(string)
-    for i in range(0,len1,1):
-    	if string[i] == ['!', '@', '#', '$', '%', '^', '&', '*']:
-    		
-    		
-
-
+    temp = ""
+    for i in range(0, len1, 1):
+        temp = string[i]
+        if temp == string1[i]:
+            temp == " "
+            newstr += temp
+    print(newstr)
 if __name__ == "__main__":
     main()
