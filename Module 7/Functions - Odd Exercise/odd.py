3ee9fa51b2fe86e12b3b553cb86310de3383d64e
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-06 19:01:29 +0530

    Odd

---
+++ Module 7/Functions - Odd Exercise/odd.py
@@ -0,0 +1,26 @@
+'''Exercise: odd'''
+
+# Write a Python function, odd, that takes in one number and returns True
+# when the number is odd and False otherwise.
+
+# You should use the % (mod) operator, not if.
+
+# This function takes in one number and returns a boolean.
+
+
+def odd(num):
+    '''
+    x: int or float.
+
+    returns: True if x is odd, False otherwise
+    '''
+    # Your code here
+    return bool(num % 2)
+
+def main():
+    '''odd number'''
+    data = input()
+    print(odd(int(data)))
+
+if __name__ == "__main__":
+    main()
