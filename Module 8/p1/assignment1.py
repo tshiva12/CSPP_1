f16634e7f9ccb8169c5e3abf47951b3c7b33a823
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-07 15:19:47 +0530

    " -> 8 of 10 passed. pylint: 9.17/10 "

---
+++ Module 8/p1/assignment1.py
@@ -0,0 +1,22 @@
+'''Exercise: Assignment-1'''
+# Write a Python function, factorial(n), that takes in one number and
+# returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+
+def factorial(num):
+    '''the factorial of n'''
+    # Your code here
+    if num == 1:
+        return 1
+    if num != 1:
+        return num * factorial(num-1)
+def main():
+    '''fact'''
+    val = input()
+    print(factorial(int(val)))
+if __name__ == "__main__":
+    import sys
+    sys.setrecursionlimit(25500)
+    main()
