36c2d7de68b0502ce12a3d565c2e4bbd933fcde6
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-16 09:47:37 +0530

    " -> 17 of 20 passed. pylint: 9.90/10 "

--- Module 14/CodeCampPoker/poker.py
+++ Module 14/CodeCampPoker/poker.py
@@ -85,10 +85,9 @@ def is_onepair(hand):
 def is_twopair(hand):
     '''It have two pairs of same cards and one different'''
     newlist = sorted(new(hand))
-    newlist1 = newlist.copy()
+    newlist1 = set(newlist)
     if len(newlist) - len(newlist1) == 2:
-        if max(newlist):
-            return True
+        return True
     return False
 
 def is_fullhouse(hand):
@@ -104,18 +103,6 @@ def is_fullhouse(hand):
         return True
     return False
 
-'''def is_highcard(hand):
-    count = 0
-    newlist = sorted(new(hand))
-    len1 = len(newlist)
-    for i in range(len1+1):
-        if newlist[i] < newlist[i+1]:
-            count += 1
-    if count == 5:
-        if max(count):
-            return True
-    return False'''
-
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
@@ -124,22 +111,20 @@ def hand_rank(hand):
         The first version should identify if the given hand is a straight
         or a flush or a straight flush.
     '''
-    '''if is_highcard(hand):
-                    return 9'''
     if is_threeofakind(hand):
-        return 6
-    if is_onepair(hand):
-        return 8
-    if is_twopair(hand):
-        return 7
-    if is_fullhouse(hand):
         return 3
-    if is_fourofakind(hand):
-        return 2
-    if is_straight(hand) and is_flush(hand):
+    if is_onepair(hand):
         return 1
-    if is_flush(hand):
+    if is_twopair(hand):
+        return 2
+    if is_fullhouse(hand):
+        return 7
+    if is_fourofakind(hand):
         return 4
+    if is_straight(hand) and is_flush(hand):
+        return 8
+    if is_flush(hand):
+        return 6
     if is_straight(hand):
         return 5
     # By now you should have seen the way a card is represented.
