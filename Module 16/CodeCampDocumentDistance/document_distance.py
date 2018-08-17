49c11e69f00bd7e8f5da09848e3c6cbea9f0259d
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-17 15:43:09 +0530

    " -> 0 of 3 passed. pylint: 9.61/10 "

--- Module 16/CodeCampDocumentDistance/document_distance.py
+++ Module 16/CodeCampDocumentDistance/document_distance.py
@@ -10,9 +10,13 @@ def similarity(dict1, dict2):
     import math
     char = string.ascii_letters + ' '
     dict1 = "".join(ele for ele in dict1 if ele in char)
+    print(dict1)
     dict2 = "".join(ele for ele in dict2 if ele in char)
+    print(dict2)
     dict1 = dict1.lower().strip().split()
+    print(dict1)
     dict2 = dict2.lower().strip().split()
+    print(dict2)
     stopword = load_stopwords("stopwords.txt")
     for word in list(dict1):
         if word in stopword:
@@ -24,8 +28,8 @@ def similarity(dict1, dict2):
     counter2 = {}
     counter1 = collections.Counter(dict1)
     counter2 = collections.Counter(dict2)
-    # print(counter1.most_common())
-    # print(counter2.most_common())
+    print(counter1.most_common())
+    print(counter2.most_common())
     numer = []
     denom1 = []
     denom2 = []
@@ -36,18 +40,20 @@ def similarity(dict1, dict2):
         denom1.append(counter1[word]**2)
     for word in counter2:
         denom2.append(counter2[word]**2)
-    return sum(numer)/(math.sqrt(sum(denom1)) * math.sqrt(sum(denom2)))
+    Numerator = sum(numer)
+    Denominator = math.sqrt(sum(denom1)) * math.sqrt(sum(denom2))
+    return Numerator/Denominator
 
 
 
 
-def load_stopwords(filename):
+def load_stopwords(_):
     '''
         loads stop words from a file and returns a dictionary
     '''
     stopwords = {}
-    with open(filename, 'r') as filename:
-        for line in filename:
+    with open(_, 'r') as _:
+        for line in _:
             stopwords[line.strip()] = 0
     return stopwords
 
