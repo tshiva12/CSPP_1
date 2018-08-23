75aa008565632fec0b6d90804b0f58e60ade718f
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-23 15:20:15 +0530

    " -> 1 of 2 passed. pylint: 9.68/10 "

--- Module 12 mid 2/p2/social_network.py
+++ Module 12 mid 2/p2/social_network.py
@@ -14,7 +14,12 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    pass
+    if arg1 in network:
+        network[arg1].append(arg2)
+    else:
+        network[arg1] = arg2
+    return network
+
 
 def unfollow(network, arg1, arg2):
     '''
@@ -26,7 +31,8 @@ def unfollow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    pass
+    network[arg1].remove(arg2)
+    return network
 
 def delete_person(network, arg1):
     '''
@@ -39,7 +45,13 @@ def delete_person(network, arg1):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    pass
+    for i in network:
+        if arg1 in network[i]:
+            network[i].remove(arg1)
+    if arg1 in network:
+        network.pop(arg1)
+    return network
+
 
 def main():
     '''
