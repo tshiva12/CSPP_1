03ecb36a306f1f811d5d168ec1c58468a1252a98
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-23 14:38:00 +0530

    " -> 7 of 8 passed. pylint: 8.80/10 "

--- Module 20/CodeCampMatrixOperations/matrix_operations.py
+++ Module 20/CodeCampMatrixOperations/matrix_operations.py
@@ -8,19 +8,20 @@ def mult_matrix(matrix1, matrix2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    if len(matrix1) != len(matrix2[0]):
+    if len(matrix1) == len(matrix2[0]):
+        list1 = []
+        for i in range(0, len(matrix1), 1):
+            list2 = []
+            for j in range(0, len(matrix2[0]), 1):
+                result = 0
+                for k in range(0, len(matrix2), 1):
+                    result += int(matrix1[i][k]) * int(matrix2[k][j])
+                list2.append(result)
+            list1.append(list2)
+        return list1
+    else:
         print("Error: Matrix shapes invalid for mult")
         return None
-    list1 = []
-    for i in range(0, len(matrix1), 1):
-        list2 = []
-        for j in range(0, len(matrix2[0]), 1):
-            result = 0
-            for k in range(0, len(matrix2), 1):
-                result += int(matrix1[i][k]) * int(matrix2[k][j])
-            list2.append(result)
-        list1.append(list2)
-    return list1
     # print("matrix m1 is :")
     # for r in matrix1:
     #   print(r)
