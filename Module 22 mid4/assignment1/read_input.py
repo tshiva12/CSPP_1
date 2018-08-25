e475a69b31e9debd47b5540510fd36898dafba1b
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-25 11:28:32 +0530

    " -> 2 of 2 passed. pylint: 8.75/10 "

--- Module 22 mid4/assignment1/read_input.py
+++ Module 22 mid4/assignment1/read_input.py
@@ -1,19 +1,14 @@
 '''
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
-def result(string):
-    '''result of string after updating into one string'''
-    print("\n")
-    return string
 def main():
     '''How to take input for the given assignment'''
     # Main function of string
     string = ""
     num = int(input())
     for i in range(num):
-        string += str(input())
-        i += 1
-    result(string)
+        string += str(input()) + "\n"
+        # i += 1
     print(string)
 
 if __name__ == '__main__':
