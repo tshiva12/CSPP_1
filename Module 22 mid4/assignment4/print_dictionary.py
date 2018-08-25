e89347a427471950aa0c4e0665dba05cf6b54bdf
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-25 12:35:35 +0530

    " -> 3 of 3 passed. pylint: 8.00/10 "

--- Module 22 mid4/assignment4/print_dictionary.py
+++ Module 22 mid4/assignment4/print_dictionary.py
@@ -6,8 +6,10 @@ the key and frequency with a SPACE - SPACE.
 
 def print_dictionary(dictionary):
     '''print dictionary'''
-    for i in dictionary:
-        print(i+' '+'-'+' '+ str(dictionary[i]))
+    list1 = list(dictionary.keys())
+    list1.sort()
+    for i in range(len(list1)):
+        print(list1[i], "-", dictionary[list1[i]], end=('\n'))
 
 def main():
     '''Main function'''
