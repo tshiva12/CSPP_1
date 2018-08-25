9053daddb868b812a34ae8c1e25263829dcca80c
tshiva12 <shivaprasad233307@msitprogram.net>
2018-08-25 12:25:22 +0530

    " -> 3 of 4 passed. pylint: 7.78/10 "

--- Module 22 mid4/assignment3/tokenize.py
+++ Module 22 mid4/assignment3/tokenize.py
@@ -3,23 +3,26 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 import re
-dict1 = {}
+dictionary = {}
 def tokenize(string):
     '''Take the count of each word in a string'''
     list_1 = string.split(' ')
     for index in list_1:
         word = re.sub('[^a-zA-Z0-9]', '', index)
-        if word in dict1:
-            dict1[word] += 1
+        if word in dictionary:
+            dictionary[word] += 1
         else:
-            dict1[word] = 1
-    return dict1
+            dictionary[word] = 1
+    return dictionary
+            
 def main():
     '''Main function'''
     num = int(input())
-    for _ in range(num):
+    for i in range(num):
         string = input()
     tokenize(string)
-    print(dict1)
+    print(dictionary)
+    
+
 if __name__ == '__main__':
     main()
