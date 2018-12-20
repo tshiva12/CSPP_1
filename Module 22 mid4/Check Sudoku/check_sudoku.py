209db9f21d0dd0cb413dfc1f5619ed71a069e8f4
tshiva12 <shivaprasad233307@msitprogram.net>
2018-12-20 22:02:10 +0530

    " -> 26 of 26 passed. pylint: 9.09/10 "

--- Module 22 mid4/Check Sudoku/check_sudoku.py
+++ Module 22 mid4/Check Sudoku/check_sudoku.py
@@ -15,15 +15,15 @@ def check_sudoku(sudoku):
     '''
     row, column = 0, 0
     for i in range(len(sudoku)):
-        if(len(set(sudoku[i])) == 9):
+        if len(set(sudoku[i])) == 9:
             row += 1
     for i in range(len(sudoku)):
         columnlist = []
         for j in range(len(sudoku)):
             columnlist.append(sudoku[j][i])
-        if (len(set(columnlist)) == 9):
+        if len(set(columnlist)) == 9:
             column += 1
-    if (row == 9 & column == 9):
+    if row == 9 & column == 9:
         return True
     return False
 
