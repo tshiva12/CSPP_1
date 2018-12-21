a13f18b2db4b4b683d7f2391d8fed0ed84631cd7
tshiva12 <shivaprasad233307@msitprogram.net>
2018-12-21 10:03:25 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 5/10 "

---
+++ Remidial Exam/CSPP-1_Question/CSPP-1_Question/Solution.py
@@ -0,0 +1,23 @@
+def Sudoku(sudoku):
+    row, column = 0, 0
+    for i in range(len(sudoku)):
+        if len(set(sudoku[i])) == 9:
+            row += 1
+    for i in range(len(sudoku)):
+        columnlist = []
+        for j in range(len(sudoku)):
+            columnlist.append(sudoku[j][i])
+        if len(set(columnlist)) == 9:
+            column += 1
+    if row == 9 & column == 9:
+        print("Given sudoku is solved")
+
+def main():
+    sudoku = []
+    for i in range(1):
+        row = input()
+        sudoku.append(row)
+    Sudoku(sudoku)
+
+if __name__ == '__main__':
+    main()
